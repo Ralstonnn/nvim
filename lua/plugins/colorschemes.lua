3416f5c3e6eec19aee70d9a93a488d2ae2175f13
Ralstonnn <zigthehedge2002@gmail.com>
2024-01-23 23:38:26 +0500

    changed themes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,26 +1,51 @@
 return {
---	{
---    "yorik1984/newpaper.nvim",
---    priority = 1000,
---    config = true,
---		opts = {
---			style = "dark"
---		}
---	},
---	{ 
---		"ellisonleao/gruvbox.nvim", 
---		priority = 1000, 
---    config = function ()
---			vim.o.background = "dark" -- or "light" for light mode
---			vim.cmd.colorscheme "gruvbox"
---		end,
---	},
+	-- {
+	-- 	"yorik1984/newpaper.nvim",
+	-- 	priority = 1000,
+	-- 	config = true,
+	-- 	opts = {
+	-- 		style = "dark"
+	-- 	}
+	-- },
+	-- { 
+	-- 	"ellisonleao/gruvbox.nvim", 
+	-- 	priority = 1000, 
+	-- 	config = function ()
+	-- 		vim.o.background = "dark" -- or "light" for light mode
+	-- 		vim.cmd.colorscheme "gruvbox"
+	-- 	end,
+	-- },
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
-    config = function ()
+		config = function ()
+			local catppuccin = require("catppuccin")
+			catppuccin.setup({
+				flavour = "mocha"
+			})
 			vim.cmd.colorscheme "catppuccin"
 		end,
-	}
+	},
+	-- {
+	-- 	"baliestri/aura-theme",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function(plugin)
+	-- 		vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+	-- 		vim.cmd([[colorscheme aura-dark]])
+	-- 	end
+	-- },
+	-- {
+	-- 	{
+	-- 		{
+	-- 			'kepano/flexoki-neovim',
+	-- 			name = 'flexoki',
+	-- 			config = function ()
+	-- 				vim.cmd('colorscheme flexoki-dark')
+	-- 				-- vim.cmd('colorscheme flexoki-light')
+	-- 			end
+	-- 		}
+	-- 	}
+	-- }
 }
