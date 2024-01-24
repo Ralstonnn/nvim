0222d059c2e678f55fbcd0a848a149f1a51cb26c
Ralstonnn <zigthehedge2002@gmail.com>
2024-01-24 22:05:30 +0500

    Aded undotree and todo-comments plugin

---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		'mbbill/undotree',
+		config = function()
+			vim.keymap.set('n', '<leader><F5>', function()
+				vim.cmd.UndotreeToggle()
+				vim.cmd.UndotreeFocus()
+			end)
+		end
+	}
+}
