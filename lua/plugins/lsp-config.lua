61e4e3292f4de1f3b90554418499f2eb9252ca01
Ralstonnn <zigthehedge2002@gmail.com>
2024-03-27 21:35:53 +0500

    Updated volar and added vue plugin to ts

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -45,7 +45,7 @@ return {
 				"html",
 				"jsonls",
 				"tailwindcss",
-				"tsserver",
+				"volar",
 				"sqlls",
 				"jedi_language_server",
 			}
@@ -57,32 +57,48 @@ return {
 				})
 			end
 
-			-- NOTE: Get typescript project-local or take global version volar
-			local util = require("lspconfig.util")
-			local function get_typescript_server_path(root_dir)
-				local global_ts = home
-					.. "/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib"
-				local found_ts = ""
-				local function check_dir(path)
-					found_ts = util.path.join(path, "node_modules", "typescript", "lib")
-					if util.path.exists(found_ts) then
-						return path
-					end
-				end
-				if util.search_ancestors(root_dir, check_dir) then
-					return found_ts
-				else
-					return global_ts
-				end
-			end
+			-- Change to yout current path
+			local vue_language_server_path = "/Users/ralstonnn/.local/share/nvim/mason/packages/typescript-language-server/node_modules/@vue/typescript-plugin"
+			lspconfig.tsserver.setup {
+				init_options = {
+					plugins = {
+						{
+							name = '@vue/typescript-plugin',
+							location = vue_language_server_path,
+							languages = { 'vue' },
+						},
+					},
+				},
+				filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+			}
 
-			lspconfig.volar.setup({
-				filetypes = { "vue" },
-				capabilities = capabilities,
-				on_new_config = function(new_config, new_root_dir)
-					new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
-				end,
-			})
+			-- NOTE: Get typescript project-local or take global version volar
+			
+			-- local util = require("lspconfig.util")
+			-- local function get_typescript_server_path(root_dir)
+			-- 	local global_ts = home
+			-- 		.. "/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib"
+			-- 	local found_ts = ""
+			-- 	local function check_dir(path)
+			-- 		found_ts = util.path.join(path, "node_modules", "typescript", "lib")
+			-- 		if util.path.exists(found_ts) then
+			-- 			return path
+			-- 		end
+			-- 	end
+			-- 	if util.search_ancestors(root_dir, check_dir) then
+			-- 		return found_ts
+			-- 	else
+			-- 		return global_ts
+			-- 	end
+			-- end
+			--
+			-- lspconfig.volar.setup({
+			-- 	filetypes = { "vue" },
+			-- 	capabilities = capabilities,
+			-- 	on_new_config = function(new_config, new_root_dir)
+			-- 		new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+			-- 	end,
+			-- })
 			-------------------------------------------------------------------
 
 			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
