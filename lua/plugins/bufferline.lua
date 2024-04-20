14fa058877e44feceba9aba6445c385e1b4770c5
Ralstonnn <zigthehedge2002@gmail.com>
2024-04-20 20:00:06 +0500

    bufferline corrections

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -30,15 +30,15 @@ return {
 			bufferline.setup({
 				options = {
 					numbers = "ordinal",
-					diagnostics = "nvim_lsp",
-					diagnostics_indicator = function(count, level, diagnostics_dict, context)
-						local s = " "
-						for e, n in pairs(diagnostics_dict) do
-							local sym = e == "error" and " " or (e == "warning" and " " or "")
-							s = s .. n .. sym
-						end
-						return s
-					end,
+					-- diagnostics = "nvim_lsp",
+					-- diagnostics_indicator = function(count, level, diagnostics_dict, context)
+					-- 	local s = " "
+					-- 	for e, n in pairs(diagnostics_dict) do
+					-- 		local sym = e == "error" and " " or (e == "warning" and " " or "")
+					-- 		s = s .. n .. sym
+					-- 	end
+					-- 	return s
+					-- end,
 
 					separator_style = "slant",
 					-- indicator = { style = "underline" },
