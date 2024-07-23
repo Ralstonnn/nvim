bccb3b12845c226492c381acd04a052a4151f863
Ralstonnn <zigthehedge2002@gmail.com>
2024-07-23 12:24:19 +0500

    Update which-key config

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -6,18 +6,18 @@ return {
 			require("which-key").setup()
 
 			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-				["<leader>h"] = { name = "[H]arpoon", _ = "which_key_ignore" },
-				["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-				["<leader>b"] = { name = "[B]uffer", _ = "which_key_ignore" },
-				["<leader>bc"] = { name = "[B]uffer [C]lose", _ = "which_key_ignore" },
-				["<leader>bm"] = { name = "[B]ufferLine [M]ove", _ = "which_key_ignore" },
-				["<leader><F5>"] = { name = "UndoTree", _ = "which_key_ignore" },
+			require("which-key").add({
+				{ "<leader><F5>", group = "UndoTree" },
+				{ "<leader>b", group = "[B]uffer" },
+				{ "<leader>bc", group = "[B]uffer [C]lose" },
+				{ "<leader>bm", group = "[B]ufferLine [M]ove" },
+				{ "<leader>c", group = "[C]ode" },
+				{ "<leader>d", group = "[D]ocument" },
+				{ "<leader>g", group = "[G]it" },
+				{ "<leader>h", group = "[H]arpoon" },
+				{ "<leader>r", group = "[R]ename" },
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>w", group = "[W]orkspace" },
 			})
 		end,
 	},
