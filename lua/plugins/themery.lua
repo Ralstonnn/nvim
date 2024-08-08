25f0a00013bdb088614748335acad5d69c1f88cf
Ralstonnn <zigthehedge2002@gmail.com>
2024-08-08 22:47:51 +0500

    themery theme fix

--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -97,7 +97,7 @@ return {
 						colorscheme = "tokyonight",
 						before = [[
 							require('tokyonight').setup({
-							    style = "Day"
+							    style = "day"
 							})
 						]],
 					},
