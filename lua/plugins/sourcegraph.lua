998b21de96e0f4ea1db353d182ed41bee6c69ae0
ralstonnn <zigthehedge2002@gmail.com>
2024-10-26 21:59:11 +0500

    added keymap to toggle cody

--- lua/plugins/sourcegraph.lua
+++ lua/plugins/sourcegraph.lua
@@ -2,6 +2,12 @@ return {
 	{
 		"sourcegraph/sg.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
-		config = true,
+		config = function()
+			local sg = require("sg")
+
+			vim.keymap.set("n", "<leader>cot", "<cmd>CodyToggle<cr>")
+
+			sg.setup({})
+		end,
 	},
 }
