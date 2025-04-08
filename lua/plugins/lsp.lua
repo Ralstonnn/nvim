5e8f1186b1e4eaa54cad16c64b36afc2dd519f93
ralstonnn <zigthehedge2002@gmail.com>
2025-04-09 01:46:39 +0500

    added inlay-hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -74,6 +74,8 @@ return {
 			-- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
 			-- used for completion, annotations and signatures of Neovim apis
 			{ "folke/neodev.nvim", opts = {} },
+			-- NOTE: Plugin that gives function parameters hints
+			{ "MysticalDevil/inlay-hints.nvim", opts = {} },
 		},
 		config = function()
 			local lspconfig = require("lspconfig")
