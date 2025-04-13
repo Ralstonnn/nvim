59ce28fcde2e671dc9384ea89b773eb36be971c3
ralstonnn <zigthehedge2002@gmail.com>
2025-04-14 00:06:09 +0500

    update

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -3,45 +3,29 @@ return {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
       {
         "L3MON4D3/LuaSnip",
         build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
           if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
             return
           end
           return "make install_jsregexp"
         end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          {
-            "rafamadriz/friendly-snippets",
-            config = function()
-              require("luasnip.loaders.from_vscode").lazy_load()
-            end,
-          },
-        },
       },
-      "saadparwaiz1/cmp_luasnip",
-
-      -- Adds other completion capabilities.
-      --  nvim-cmp does not ship with all sources by default. They are split
-      --  into multiple repos for maintenance purposes.
+      "rafamadriz/friendly-snippets", -- useful snippets
+      "saadparwaiz1/cmp_luasnip", -- for autocompletion
       "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-path",
-      "onsails/lspkind.nvim",
+      "hrsh7th/cmp-buffer", -- source for text in buffer
+      "hrsh7th/cmp-path", -- source for file system paths
+      "onsails/lspkind.nvim", -- vs-code like pictograms
     },
     config = function()
       -- See `:help cmp`
       local cmp = require("cmp")
       local luasnip = require("luasnip")
       local lspkind = require("lspkind")
-      luasnip.config.setup({})
+
+      require("luasnip.loaders.from_vscode").lazy_load()
 
       local formatting_style = {
         format = lspkind.cmp_format({
@@ -117,9 +101,9 @@ return {
         }),
         sources = {
           { name = "nvim_lsp" },
-          { name = "cody" },
-          { name = "luasnip" },
-          { name = "path" },
+          { name = "luasnip" }, -- snippets
+          { name = "buffer" }, -- text within current buffer
+          { name = "path" }, -- file system paths
         },
       })
     end,
