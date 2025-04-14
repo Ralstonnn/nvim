56e6417f0786159798612cd03d875cf1ed03d5fd
ralstonnn <zigthehedge2002@gmail.com>
2025-04-14 17:12:49 +0500

    updated keymaps and snacks gitbrowse pattern

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,8 +2,8 @@ local keymap = vim.keymap
 
 keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>")
 keymap.set("i", "jj", "<esc>")
-keymap.set("n", "<c-d>", "<c-d>zz")
-keymap.set("n", "<c-u>", "<c-u>zz")
+-- keymap.set("n", "<c-d>", "<c-d>zz")
+-- keymap.set("n", "<c-u>", "<c-u>zz")
 
 -- diagnostic keymaps
 keymap.set("n", "[d", function()
