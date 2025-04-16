da55796b9962f5196f94d5e96ef5bae9dd5ae31a
ralstonnn <zigthehedge2002@gmail.com>
2025-04-16 17:21:49 +0500

    changed themes

--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -13,6 +13,7 @@ return {
             name = "Gruvbox Original Dark",
             colorscheme = "gruvbox-material",
             before = [[
+              vim.opt.background = "dark"
               vim.g.gruvbox_material_foreground = "original"
             ]],
           },
@@ -20,6 +21,7 @@ return {
             name = "Gruvbox Material Dark",
             colorscheme = "gruvbox-material",
             before = [[
+              vim.opt.background = "dark"
               vim.g.gruvbox_material_foreground = "material"
             ]],
           },
@@ -27,6 +29,7 @@ return {
             name = "Gruvbox Mix Dark",
             colorscheme = "gruvbox-material",
             before = [[
+              vim.opt.background = "dark"
               vim.g.gruvbox_material_foreground = "mix"
             ]],
           },
@@ -35,8 +38,25 @@ return {
           -- ROSE PINE
           -- ********************************************************************************
           {
-            name = "Rose Pine Dark",
-            colorscheme = "rose-pine",
+            name = "Rose Pine",
+            colorscheme = "rose-pine-main",
+            before = [[
+              vim.opt.background = "dark"
+            ]],
+          },
+          {
+            name = "Rose Pine Moon",
+            colorscheme = "rose-pine-moon",
+            before = [[
+              vim.opt.background = "dark"
+            ]],
+          },
+          {
+            name = "Rose Pine Dawn",
+            colorscheme = "rose-pine-dawn",
+            before = [[
+              vim.opt.background = "light"
+            ]],
           },
 
           -- ********************************************************************************
@@ -46,6 +66,7 @@ return {
             name = "Catppuccin Mocha",
             colorscheme = "catppuccin",
             before = [[
+              vim.opt.background = "dark"
               require("catppuccin").setup({
                 flavour = "mocha",
               })
@@ -55,6 +76,7 @@ return {
             name = "Catppuccin Transparent",
             colorscheme = "catppuccin",
             before = [[
+              vim.opt.background = "dark"
               require("catppuccin").setup({
                 flavour = "mocha",
                 transparent_background = true,
@@ -63,94 +85,62 @@ return {
           },
 
           -- ********************************************************************************
-          -- ONE DARK PRO
-          -- ********************************************************************************
-          {
-            name = "One Dark Pro Light",
-            colorscheme = "onelight",
-            before = [[
-              require('onedarkpro').setup({})
-            ]],
-          },
-          {
-            name = "One Dark Pro",
-            colorscheme = "onedark",
-            before = [[
-              require('onedarkpro').setup({})
-            ]],
-          },
-          {
-            name = "One Dark Pro Vivid",
-            colorscheme = "onedark_vivid",
-            before = [[
-              require('onedarkpro').setup({})
-            ]],
-          },
-          {
-            name = "One Dark Pro Dark",
-            colorscheme = "onedark_dark",
-            before = [[
-              require('onedarkpro').setup({})
-            ]],
-          },
-
-          -- ********************************************************************************
           -- ONE DARK
           -- ********************************************************************************
-          -- {
-          -- 	name = "One Dark Warm",
-          -- 	colorscheme = "onedark",
-          -- 	before = [[
-          -- 		require('onedark').setup({
-          -- 		    style = "warm"
-          -- 		})
-          -- 	]],
-          -- },
-          -- {
-          -- 	name = "One Dark Warmer",
-          -- 	colorscheme = "onedark",
-          -- 	before = [[
-          -- 		require('onedark').setup({
-          -- 		    style = "warmer"
-          -- 		})
-          -- 	]],
-          -- },
-          -- {
-          -- 	name = "One Dark Cool",
-          -- 	colorscheme = "onedark",
-          -- 	before = [[
-          -- 		require('onedark').setup({
-          -- 		    style = "cool"
-          -- 		})
-          -- 	]],
-          -- },
-          -- {
-          -- 	name = "One Dark Deep",
-          -- 	colorscheme = "onedark",
-          -- 	before = [[
-          -- 		require('onedark').setup({
-          -- 		    style = "deep"
-          -- 		})
-          -- 	]],
-          -- },
-          -- {
-          -- 	name = "One Dark Dark",
-          -- 	colorscheme = "onedark",
-          -- 	before = [[
-          -- 		require('onedark').setup({
-          -- 		    style = "dark"
-          -- 		})
-          -- 	]],
-          -- },
-          -- {
-          -- 	name = "One Dark Darker",
-          -- 	colorscheme = "onedark",
-          -- 	before = [[
-          -- 		require('onedark').setup({
-          -- 		    style = "darker"
-          -- 		})
-          -- 	]],
-          -- },
+          {
+            name = "One Dark Warm",
+            colorscheme = "onedark",
+            before = [[
+          		require('onedark').setup({
+          		    style = "warm"
+          		})
+          	]],
+          },
+          {
+            name = "One Dark Warmer",
+            colorscheme = "onedark",
+            before = [[
+          		require('onedark').setup({
+          		    style = "warmer"
+          		})
+          	]],
+          },
+          {
+            name = "One Dark Cool",
+            colorscheme = "onedark",
+            before = [[
+          		require('onedark').setup({
+          		    style = "cool"
+          		})
+          	]],
+          },
+          {
+            name = "One Dark Deep",
+            colorscheme = "onedark",
+            before = [[
+          		require('onedark').setup({
+          		    style = "deep"
+          		})
+          	]],
+          },
+          {
+            name = "One Dark Dark",
+            colorscheme = "onedark",
+            before = [[
+          		require('onedark').setup({
+          		    style = "dark"
+          		})
+          	]],
+          },
+          {
+            name = "One Dark Darker",
+            colorscheme = "onedark",
+            before = [[
+          		require('onedark').setup({
+          		    style = "darker"
+          		})
+          	]],
+          },
 
           -- ********************************************************************************
           -- TOKYO NIGHT
@@ -159,6 +149,7 @@ return {
             name = "Tokyo Night Night",
             colorscheme = "tokyonight",
             before = [[
+              vim.opt.background = "dark"
               require('tokyonight').setup({
                 style = "night"
               })
@@ -168,6 +159,7 @@ return {
             name = "Tokyo Night Storm",
             colorscheme = "tokyonight",
             before = [[
+              vim.opt.background = "dark"
               require('tokyonight').setup({
                 style = "storm"
               })
@@ -177,6 +169,7 @@ return {
             name = "Tokyo Night Moon",
             colorscheme = "tokyonight",
             before = [[
+              vim.opt.background = "dark"
               require('tokyonight').setup({
                 style = "moon"
               })
@@ -186,9 +179,110 @@ return {
             name = "Tokyo Night Day",
             colorscheme = "tokyonight",
             before = [[
+              vim.opt.background = "light"
               require('tokyonight').setup({
                 style = "day"
               })
+            ]],
+          },
+
+          -- ********************************************************************************
+          -- OXOCARBON
+          -- ********************************************************************************
+          {
+            name = "Oxocarbon",
+            colorscheme = "oxocarbon",
+            before = [[
+              vim.opt.background = "dark"
+            ]],
+          },
+
+          -- ********************************************************************************
+          -- NIGHTFOX
+          -- ********************************************************************************
+          {
+            name = "Nightfox",
+            colorscheme = "nightfox",
+            before = [[
+              vim.opt.background = "dark"
+            ]],
+          },
+          {
+            name = "Dayfox",
+            colorscheme = "dayfox",
+            before = [[
+              vim.opt.background = "light"
+            ]],
+          },
+          {
+            name = "Dawnfox",
+            colorscheme = "dawnfox",
+            before = [[
+              vim.opt.background = "light"
+            ]],
+          },
+          {
+            name = "Duskfox",
+            colorscheme = "duskfox",
+            before = [[
+              vim.opt.background = "dark"
+            ]],
+          },
+          {
+            name = "Nordfox",
+            colorscheme = "nordfox",
+            before = [[
+              vim.opt.background = "dark"
+            ]],
+          },
+          {
+            name = "Terafox",
+            colorscheme = "terafox",
+            before = [[
+              vim.opt.background = "dark"
+            ]],
+          },
+          {
+            name = "Carbonfox",
+            colorscheme = "carbonfox",
+            before = [[
+              vim.opt.background = "dark"
+            ]],
+          },
+
+          -- ********************************************************************************
+          -- NORD
+          -- ********************************************************************************
+          {
+            name = "Nord",
+            colorscheme = "nord",
+            before = [[
+              vim.opt.background = "dark"
+            ]],
+          },
+
+          -- ********************************************************************************
+          -- KANAGAWA
+          -- ********************************************************************************
+          {
+            name = "Kanagawa Wave",
+            colorscheme = "kanagawa-wave",
+            before = [[
+              vim.opt.background = "dark"
+            ]],
+          },
+          {
+            name = "Kanagawa Dragon",
+            colorscheme = "kanagawa-dragon",
+            before = [[
+              vim.opt.background = "dark"
+            ]],
+          },
+          {
+            name = "Kanagawa Lotus",
+            colorscheme = "kanagawa-lotus",
+            before = [[
+              vim.opt.background = "light"
             ]],
           },
         }, -- Your list of installed colorschemes.
@@ -197,9 +291,12 @@ return {
     end,
   },
   "folke/tokyonight.nvim",
-  -- "navarasu/onedark.nvim",
-  "olimorris/onedarkpro.nvim",
+  "navarasu/onedark.nvim",
   "sainnhe/gruvbox-material",
   "rose-pine/neovim",
   "catppuccin/nvim",
+  "EdenEast/nightfox.nvim",
+  "gbprod/nord.nvim",
+  "rebelot/kanagawa.nvim",
+  "nyoom-engineering/oxocarbon.nvim",
 }
