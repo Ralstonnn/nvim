c97ca1fd5cfa0de4e9294d2ba76a4dbe1e42cd0b
Ralstonnn <zigthehedge2002@gmail.com>
2025-04-23 22:55:21 +0500

    added macro to log faster in js ts jsx tsx vue

---
+++ lua/macros/javascript.lua
@@ -0,0 +1,12 @@
+local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
+
+vim.api.nvim_create_augroup("JsLogMacro", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = "JsLogMacro",
+  pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+  callback = function()
+    vim.fn.setreg("l", "yiwoconsole.log('" .. esc .. "pa: ', " .. esc .. "pa);" .. esc)
+    vim.fn.setreg("o", "yiwoconsole.log('" .. esc .. "pviwUA:');" .. esc .. "oconsole.log(" .. esc .. "pA);" .. esc)
+  end,
+})
