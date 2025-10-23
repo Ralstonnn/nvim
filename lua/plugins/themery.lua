3dc4634d5f603e91f3199cd78f61bdd5bf440736
Ralstonnn <zigthehedge2002@gmail.com>
2025-10-24 01:54:10 +0500

    init

---
+++ lua/plugins/themery.lua
@@ -0,0 +1,12 @@
+return {
+  "zaldih/themery.nvim",
+  lazy = false,
+  priority = 1000,
+  dependencies = {
+    "olimorris/onedarkpro.nvim",
+  },
+  opts = {
+    themes= {"onedark"},
+    listPreview = true,
+  },
+}
