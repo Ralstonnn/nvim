d8aa8e20e5a0c8440658efdd0fc12ada716ec44a
ralstonnn <zigthehedge2002@gmail.com>
2025-10-28 15:43:10 +0500

    added config to open trouble symbols as 30% of current window

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,6 +1,15 @@
 return {
   "folke/trouble.nvim",
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  opts = {
+    modes = {
+      symbols = {
+        focus = true,
+        win = {
+          size = 0.3,
+        },
+      },
+    },
+  }, -- for default options, refer to the configuration section for custom setup.
   cmd = "Trouble",
   keys = {
     {
@@ -22,6 +31,6 @@ return {
       "<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
       desc = "Show Diagnostic [E]rror Messages Current Buffer",
     },
-    { "<leader>ls", "<cmd>Trouble symbols toggle focus=false<CR>", desc = "Symbols (Trouble)" },
+    { "<leader>ls", "<cmd>Trouble symbols toggle<CR>", desc = "Symbols (Trouble)" },
   },
 }
