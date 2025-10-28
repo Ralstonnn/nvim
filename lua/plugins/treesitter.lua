8888efb61f4a6e54724360f6420b66d343d4a9fb
ralstonnn <zigthehedge2002@gmail.com>
2025-10-28 15:30:31 +0500

    moved to old version of treesitter
    the new version in main doesn't work with vue and ts so had to move to
    the old one

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -30,10 +30,23 @@ return { -- Highlight, edit, and navigate code
       "vue",
       "typescript",
       "javascript",
-      "regex",
-      "css",
     },
+    -- Autoinstall languages that are not installed
+    auto_install = true,
+    highlight = {
+      enable = true,
+      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+      --  If you are experiencing weird indenting issues, add the language to
+      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+      additional_vim_regex_highlighting = { "ruby" },
+    },
+    indent = { enable = true },
   },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
+
+  -- NOTE: Config function for main branch
   -- config = function()
   --   local treesitter = require("nvim-treesitter")
   --
