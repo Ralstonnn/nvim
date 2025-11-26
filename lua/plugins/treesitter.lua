f9dad47a92ac425c658f65d37e024d4a97d8dc3d
ralstonnn <zigthehedge2002@gmail.com>
2025-11-26 21:07:45 +0500

    fix typo

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,5 @@
--- NOTE: Moved to old version of treesitter in master branch because the new one in main cant work with vue (doesn't insall parsers)
+-- NOTE: Moved to old version of treesitter in master branch because the new one in main cant work with vue (doesn't install parsers)
 local treesitter_master_branch_config = require("config.treesitter.master_branch_config")
-
 local treesitter_main_branch_config = require("config.treesitter.main_branch_config")
 
 local use_main_branch = true
