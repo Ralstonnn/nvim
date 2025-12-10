29c5dfdf4cdf0941d9a6d1121a6fe03dc15a77e5
ralstonnn <zigthehedge2002@gmail.com>
2025-12-10 19:16:09 +0500

    changed treesitter back to master branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,6 @@
 local treesitter_master_branch_config = require("config.treesitter.master_branch_config")
 local treesitter_main_branch_config = require("config.treesitter.main_branch_config")
 
-local use_main_branch = true
+local use_main_branch = false
 
 return use_main_branch and treesitter_main_branch_config or treesitter_master_branch_config
