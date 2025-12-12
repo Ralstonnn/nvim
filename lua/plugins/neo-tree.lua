8f9eea1316a4bd331d54a501e5da297fc44d3fec
ralstonnn <zigthehedge2002@gmail.com>
2025-12-13 03:39:28 +0500

    changed neo-tree to use rename from snacks

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,20 +5,24 @@ return {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
+      "folke/snacks.nvim",
     },
     lazy = false, -- neo-tree will lazily load itself
     keys = {
       { "<leader>nt", "<cmd>Neotree toggle<cr>", desc = "NeoTree Toggle" },
       { "<leader>nf", "<cmd>Neotree reveal<cr>", desc = "NeoTree Reveal" },
     },
-  },
-  {
-    "antosha417/nvim-lsp-file-operations",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-neo-tree/neo-tree.nvim", -- makes sure that this loads after Neo-tree.
-    },
-    opts = {},
+    opts = function(_, opts)
+      local function on_move(data)
+        Snacks.rename.on_rename_file(data.source, data.destination)
+      end
+      local events = require("neo-tree.events")
+      opts.event_handlers = opts.event_handlers or {}
+      vim.list_extend(opts.event_handlers, {
+        { event = events.FILE_MOVED, handler = on_move },
+        { event = events.FILE_RENAMED, handler = on_move },
+      })
+    end,
   },
   {
     "s1n7ax/nvim-window-picker",
