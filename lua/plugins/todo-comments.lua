9963582a4d7e30e870c2614f4c10680caa1b182d
ralstonnn <zigthehedge2002@gmail.com>
2025-12-23 15:59:17 +0500

    change todo-comments to use snacks

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -7,7 +7,20 @@ return {
       signs = false,
     },
     keys = {
-      {"<leader>st", "<cmd>TodoTelescope<CR>", desc = "[S]earch [T]odo" }
-    }
+      {
+        "<leader>st",
+        function()
+          Snacks.picker.todo_comments()
+        end,
+        desc = "Todo",
+      },
+      {
+        "<leader>sT",
+        function()
+          Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
+        end,
+        desc = "Todo/Fix/Fixme",
+      },
+    },
   },
 }
