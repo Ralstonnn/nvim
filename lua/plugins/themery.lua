a051a91db99d5b12ed7ce1107aa12c6f47c4a5d9
ralstonnn <zigthehedge2002@gmail.com>
2026-02-09 20:48:51 +0500

    added themes

--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -3,19 +3,70 @@ return {
   lazy = false,
   priority = 1000,
   dependencies = {
-    "olimorris/onedarkpro.nvim",
+    "navarasu/onedark.nvim",
     "rebelot/kanagawa.nvim",
+    "neanias/everforest-nvim",
   },
   opts = {
     themes = {
-      "onedark",
-      "onelight",
-      "onedark_vivid",
-      "onedark_dark",
-      "vaporwave",
+      {
+        name = "One Dark Warm",
+        colorscheme = "onedark",
+        before = [[
+          require('onedark').setup({
+              style = "warm"
+          })
+        ]],
+      },
+      {
+        name = "One Dark Warmer",
+        colorscheme = "onedark",
+        before = [[
+          require('onedark').setup({
+              style = "warmer"
+          })
+        ]],
+      },
+      {
+        name = "One Dark Cool",
+        colorscheme = "onedark",
+        before = [[
+          require('onedark').setup({
+              style = "cool"
+          })
+        ]],
+      },
+      {
+        name = "One Dark Deep",
+        colorscheme = "onedark",
+        before = [[
+          require('onedark').setup({
+              style = "deep"
+          })
+        ]],
+      },
+      {
+        name = "One Dark Dark",
+        colorscheme = "onedark",
+        before = [[
+          require('onedark').setup({
+              style = "dark"
+          })
+        ]],
+      },
+      {
+        name = "One Dark Darker",
+        colorscheme = "onedark",
+        before = [[
+          require('onedark').setup({
+              style = "darker"
+          })
+        ]],
+      },
       "kanagawa-wave",
       "kanagawa-dragon",
       "kanagawa-lotus",
+      "everforest",
     },
     listPreview = true,
   },
