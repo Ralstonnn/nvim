5557ec5c30b5129bf3581f35b7639e3e3267cc8a
ralstonnn <zigthehedge2002@gmail.com>
2026-06-08 12:40:22 +0500

    added kanagawa-wave-transparent

--- lua/config/themery/kanagawa.lua
+++ lua/config/themery/kanagawa.lua
@@ -1,4 +1,32 @@
 return {
-  "kanagawa-wave",
-  "kanagawa-dragon",
+  {
+    name = "kanagawa-wave",
+    colorscheme = "kanagawa-wave",
+    before = [[
+      require("kanagawa").setup({
+        theme = "wave",
+        transparent = false,
+      })
+    ]]
+  },
+  {
+    name = "kanagawa-wave-transparent",
+    colorscheme = "kanagawa-wave",
+    before = [[
+      require("kanagawa").setup({
+        theme = "wave",
+        transparent = true,
+      })
+    ]]
+  },
+  {
+    name = "kanagawa-dragon",
+    colorscheme = "kanagawa-dragon",
+    before = [[
+      require("kanagawa").setup({
+        theme = "dragon",
+        transparent = false,
+      })
+    ]]
+  },
 }
