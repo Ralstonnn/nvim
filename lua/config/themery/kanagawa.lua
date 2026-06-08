98925f35371151dbd737434c616087f865a42941
ralstonnn <zigthehedge2002@gmail.com>
2026-06-08 17:45:38 +0500

    Revert "added kanagawa-wave-transparent"
    This reverts commit 5557ec5c30b5129bf3581f35b7639e3e3267cc8a.

--- lua/config/themery/kanagawa.lua
+++ lua/config/themery/kanagawa.lua
@@ -1,32 +1,4 @@
 return {
-  {
-    name = "kanagawa-wave",
-    colorscheme = "kanagawa-wave",
-    before = [[
-      require("kanagawa").setup({
-        theme = "wave",
-        transparent = false,
-      })
-    ]]
-  },
-  {
-    name = "kanagawa-wave-transparent",
-    colorscheme = "kanagawa-wave",
-    before = [[
-      require("kanagawa").setup({
-        theme = "wave",
-        transparent = true,
-      })
-    ]]
-  },
-  {
-    name = "kanagawa-dragon",
-    colorscheme = "kanagawa-dragon",
-    before = [[
-      require("kanagawa").setup({
-        theme = "dragon",
-        transparent = false,
-      })
-    ]]
-  },
+  "kanagawa-wave",
+  "kanagawa-dragon",
 }
